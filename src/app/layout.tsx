19f80654329a8aa8c62d1bad57cdbedd8564b876
init files <>
2023-09-07 07:16:18 -0500

    chore: add deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import ResponsiveAppBar from './components/AppBar';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ResponsiveAppBar />
+        {children}
+        </body>
     </html>
   )
 }
